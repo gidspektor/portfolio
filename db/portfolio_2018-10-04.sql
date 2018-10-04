3c4b7ac9d3143be35470a42f80e1399d0da7484a
Gideon Spektor <gidspektor@gmail.com>
2018-10-04 12:15:33 +0100

    sss

---
+++ db/portfolio_2018-10-04.sql
@@ -0,0 +1,83 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.6.41)
+# Database: portfolio
+# Generation Time: 2018-10-04 11:15:19 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table about_me
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `about_me`;
+
+CREATE TABLE `about_me` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `bio` varchar(1000) DEFAULT NULL,
+  `life_now` varchar(1000) DEFAULT NULL,
+  `goals` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `about_me` WRITE;
+/*!40000 ALTER TABLE `about_me` DISABLE KEYS */;
+
+INSERT INTO `about_me` (`id`, `bio`, `life_now`, `goals`)
+VALUES
+	(1,'     Cupidatat consequat Eligendi odit sed accusamus iste sapiente deserunt nihil in voluptatum ducimus occaecat reprehenderit consectetur quibusdam illo fugiat itaque','     Dolorem voluptatum laudantium error obcaecati sit quia laborum Dolores a illum modi in incididunt consequuntur mollit tempora in','     Quia similique aliquam enim dignissimos minima cupiditate');
+
+/*!40000 ALTER TABLE `about_me` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table portfolio
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `portfolio`;
+
+CREATE TABLE `portfolio` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `project_name` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `project_url` varchar(255) DEFAULT NULL,
+  `delete` tinyint(1) unsigned DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `portfolio` WRITE;
+/*!40000 ALTER TABLE `portfolio` DISABLE KEYS */;
+
+INSERT INTO `portfolio` (`id`, `project_name`, `image`, `project_url`, `delete`)
+VALUES
+	(1,'project1','Images/bubbles.png','www.google.com',0),
+	(2,'project2','Images/bubbles.png','www.google.com',0),
+	(3,'project3','Images/bubbles.png','www.google.com',0),
+	(4,'project4','Images/bubbles.png','www.google.com',0),
+	(5,'project5','Images/bubbles.png','www.google.com',0),
+	(6,'project6','Images/git.png','www.google.com',0);
+
+/*!40000 ALTER TABLE `portfolio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
